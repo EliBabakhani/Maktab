7dd4a7f6df17d998f21ea436b071dc14fac13dff
EliBabakhani <elnaz.babakhani71@gmail.com>
2023-06-15 06:16:39 -0700

    cw11-q2

--- CW/CW11/p2.py
+++ CW/CW11/p2.py
@@ -1,2 +1,11 @@
 import argparse
 
+parser=argparse.ArgumentParser()
+
+parser.add_argument('first_num', help="add first number")
+parser.add_argument('second_num', help="add second number")
+
+args=parser.parse_args()
+print(f'sum: {int(args.first_num)+int(args.second_num)}')
+
+
